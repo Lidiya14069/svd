23636b499929b9aa5adce1fb57896fd3d62f2019
Lidiya14069 <olgejzerlidiya14@gmail.com>
2026-05-12 21:51:10 +1000

    Delete Project C directory

--- Project C/SVD/jacobi.h
+++
@@ -1,6 +0,0 @@
-#ifndef JACOBI_H
-#define JACOBI_H
-
-int jacobi_eigen(double **a, int n, double *eigenvalues, double **eigenvectors);
-
-#endif
--- Project C/utils/utils.h
+++
@@ -1,6 +0,0 @@
-#ifndef UTILS_H
-#define UTILS_H
-
-void print_line(void);
-
-#endif
